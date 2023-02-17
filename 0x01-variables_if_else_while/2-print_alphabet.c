38d65eaf59701b0bef64d2af3bfd6dea0d517e73
knwble <noble4peter1@gmail.com>
2023-02-17 14:50:19 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - print alphabet in lower case
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+
+		putchar(x);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
