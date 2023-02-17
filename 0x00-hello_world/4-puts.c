1fd59018d4578b3de89feccf5abd571f60af3d38
knwble <noble4peter1@gmail.com>
2023-02-16 16:06:02 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stio.h>
+/**
+ * main - A C program that prints with put function
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
