779062b77dfc35c8f68c77e5f6e5370e35182e03
knwble <noble4peter1@gmail.com>
2023-02-21 07:08:34 -0800

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+* print_alphabet_x10 - check the code.
+* Return: Always 0.
+*/
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+	}
+}
