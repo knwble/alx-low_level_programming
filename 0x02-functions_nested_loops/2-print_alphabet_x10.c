145c29b69987eb4f66c1033b090c05c33d160e19
knwble <noble4peter1@gmail.com>
2023-02-22 07:24:43 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,10 +12,10 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-	for (j = 'a'; j <= 'z'; j++)
-	{
-		_putchar(j);
-	}
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
 	_putchar('\n');
 	}
 }
