40243b10f767ddb90a009a3222f8b496645d6a9d
knwble <noble4peter1@gmail.com>
2023-02-23 08:05:06 -0800

    update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,9 +7,11 @@
 
 void print_numbers(void)
 {
-	int num;
-	
-	for (num = 0; num <= 9; num++)
-	_putchar((num % 10) + '0');
+	int num = 0;
+
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
 	_putchar('\n');
 }
