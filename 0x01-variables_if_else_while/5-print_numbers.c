eb9b3fa5134b898718dd54f51a586f56249889df
knwble <noble4peter1@gmail.com>
2023-02-24 05:51:42 -0800

    update

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
-* main - Entry point
+* main - a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
 * Return: Always 0 (Success)
 */
 
