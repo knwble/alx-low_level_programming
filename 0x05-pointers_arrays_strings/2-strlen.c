f0d60e47fc4f3cc249af7150a39f3e8f7f6871f8
knwble <noble4peter1@gmail.com>
2023-02-27 12:41:47 -0800

    update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+* _strlen - Entry point
+* @s: The variable to take input.
+* Return: Always 0 (Success)
+*/
+
+
+int _strlen(char *s)
+{
+	int i;
+	for (i = 0; s[i] != '\0'; i++)
+	return (i);
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+0x00. C - Hello, World
+0x01. C - Variables, if, else, while
+0x02. C - Functions, nested loops
+0x03-debugging
