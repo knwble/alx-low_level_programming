b2ba306f49f66b381237181a9c97bda38a131dc3
knwble <noble4peter1@gmail.com>
2023-02-27 13:35:40 -0800

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+* print_rev - Entry point
+* @s: The variable to take input.
+* Return: Always 0 (Success)
+*/
+
+void print_rev(char *s)
+{
+	int i;
+	
+	i = 0;
+	while (s[i] != '\0')
+	i++;
+
+	for (i = i - 1 ; i >= 0; i--)
+	_putchar (s[i]);
+	
+	_putchar ('\n');
+}
