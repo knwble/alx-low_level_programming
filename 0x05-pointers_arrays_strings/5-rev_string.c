f13539f6b74e80e89b138556a94a1d6c605e7644
knwble <noble4peter1@gmail.com>
2023-02-27 19:36:15 -0800

    5. reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+*/
+void rev_string(char *s)
+{
+	char *start_c, *end_c, c;
+	int count = 0, i;
+
+	/*find string length without null char*/
+	for (i = 0; s[i] != '\0'; i++)
+	count++;
+
+	start_c = s;
+	end_c = s;
+
+	for (i = 0; i < count - 1; i++)
+	end_c++;
+	
+
+
+	for (i = 0; i < count / 2; i++)
+	{
+
+
+		c = *end_c;
+		*end_c = *start_c;
+		*start_c = c;
+
+		start_c++;
+		end_c--;
+	}
+}
