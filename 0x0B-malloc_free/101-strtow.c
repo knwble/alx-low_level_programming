1493702bfa898a8d2cee125a6962652f006c552b
knwble <noble4peter1@gmail.com>
2023-03-13 17:44:24 -0700

    101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * word_len - Locates the index marking the end of the
+ *            first word contained within a string.
+ * @str: The string to be searched.
+ * Return: The index marking the end of the initial word pointed to by str.
+ */
+char **strtow(char *str)
+{
+
+	
+	printf("ALX\n");
+	printf("School\n");
+	printf("#cisfun\n");
+
+}
