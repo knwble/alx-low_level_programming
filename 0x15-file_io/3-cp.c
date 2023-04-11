83298c46a3a9331f2926e6a528e2d6779ecd44fe
knwble <noble4peter1@gmail.com>
2023-04-10 17:04:23 -0700

    3. cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+#define BUF_SIZE 1024
+
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: pointer to arguments
+ * Return: 0 on succe
+ */
+
+int main(int argc, char *argv[])
+{
+	int file1, file2;
+	ssize_t nread;
+	char buf[BUF_SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	file1 = open(argv[1], O_RDONLY);
+
+	if (file1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	file2 = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
+
+	if (file2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	while ((nread = read(file1, buf, BUF_SIZE)) > 0)
+	{
+		if (write(file2, buf, nread) != nread)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	if (nread == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	if (close(file1) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1);
+		exit(100);
+	}
+
+	if (close(file2) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2);
+	}
+
+	return (0);
+}
