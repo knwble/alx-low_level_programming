dcf9cb77b3a12e422ee6a2aeaef9f995b427115d
knwble <noble4peter1@gmail.com>
2023-04-10 18:03:46 -0700

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -45,8 +45,8 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	close_file(file1);
-	close_file(file2);
+	close(file1);
+	close(file2);
 
 	return (0);
 }
