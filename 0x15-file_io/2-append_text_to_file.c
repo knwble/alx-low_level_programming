0499dc967cb02253bca3661391713d864dd3c5a9
root <noble4peter1@gmail.com>
2023-05-08 14:16:45 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,25 +8,25 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int file, f_write, i;
+	int file, f_write, i;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    file = open(filename, O_APPEND | O_RDWR, 0664);
-    if (file == -1)
-        return (-1);
+	file = open(filename, O_APPEND | O_RDWR, 0664);
+	if (file == -1)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        for (i = 0; text_content[i];)
-            i+=1;
-        f_write = write(file, text_content, i);
-        if (f_write == -1)
-            return (-1);
-    }
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i];)
+			i += 1;
+		f_write = write(file, text_content, i);
+		if (f_write == -1)
+			return (-1);
+	}
 
-    close(file);
+	close(file);
 
-    return (1);
+	return (1);
 }
