7a0c370da62ef0e0f2c9addc20d49ba1096a385e
root <noble4peter1@gmail.com>
2023-05-08 14:57:01 -0700

    task 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,35 +16,36 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	file1open = open(argv[1], O_RDONLY);
 	if (file1open == -1)
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-
+		dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]), exit(98);
 	file2open = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file2open == -1)
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]), exit(99);
-
+		dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[2]), exit(99);
 	file1read = read(file1open, buffer, 1024);
 	if (file1read == -1)
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]), exit(98);
 	while (file1read > 0)
 	{
 		file1write = write(file2open, buffer, file1read);
 		if (file1write == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(100);
+			dprintf(STDERR_FILENO,
+					"Error: Can't write to %s\n", argv[2]), exit(100);
 		file1read = read(file1open, buffer, 1024);
-	if (file1read == -1)
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		if (file1read == -1)
+			dprintf(STDERR_FILENO,
+					"Error: Can't read from file %s\n", argv[1]), exit(98);
 	}
-
 	file1close = close(file1open);
 	if (file1close == -1)
-	dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", "file1open"), exit(100);
-
+		dprintf(STDERR_FILENO,
+				"Error: Can't close fd %s\n", "file1open"), exit(100);
 	file2close = close(file2open);
 	if (file2close == -1)
-	dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", "file1open"), exit(100);
-
+		dprintf(STDERR_FILENO,
+				"Error: Can't close fd %s\n", "file1open"), exit(100);
 	return (0);
 }
