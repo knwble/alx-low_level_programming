e6e9ec16db11f6dad58cd3116297109e872846b4
knwble <noble4peter1@gmail.com>
2023-07-07 11:42:57 -0700

    update

---
+++ 0x14-bit_manipulations/0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - a function that sets the value of a bit to 1 at a given index
+ * @n: a pointer to the bit to change
+ * @index: The index to set the value at - indices start at 0
+ * Return: on sucess 1
+ *         Otherwise - -1 on failure
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	*n ^= (1 << index);
+	return (1);
+}
---
+++ 0x14-bit_manipulations/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *get_endianness - a function that checks for endianness
+ * Return: 0 if big endian
+ *         1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int b = 1;
+	char *a = (char *)&b;
+
+	if (*a)
+		return (1);
+
+	return (0);
+}
