8bc5a301d254b2a7042842931bf72bd858ae675b
knwble <noble4peter1@gmail.com>
2023-08-07 14:12:28 +0100

    a program that copies the content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#define BUFSIZE 1024
+
+/**
+ * error_check - prints error messages
+ * @error: exit number or file descriptor
+ * @str: files
+ * @fd: file descriptor
+ *
+ * Return: 0 on success
+ */
+int error_check(int error, char *str, int fd)
+{
+	switch (error)
+	{
+		case 97:
+			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+			exit(error);
+		case 98:
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", str);
+			exit(error);
+		case 99:
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", str);
+			exit(error);
+		case 100:
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+			exit(error);
+		default:
+			return (0);
+	}
+}
