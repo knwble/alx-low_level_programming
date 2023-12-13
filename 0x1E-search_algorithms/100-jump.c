1e25c48a733aa5974f16e59ecc98a1f833d8daa2
knwble <noble4peter1@gmail.com>
2023-12-13 22:32:29 +0100

    update

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,54 @@
+#include "search_algos.h"
+
+size_t min(size_t a, size_t b);
+
+/**
+ * min - returns the minimum of two size_t values
+ * @a: first value
+ * @b: second value
+ *
+ * Return: `a` if lower or equal to `b`, `b` otherwise
+ */
+size_t min(size_t a, size_t b)
+{
+	return (a <= b ? a : b);
+}
+
+/**
+ * jump_search - searches for a value in a sorted array of integers using
+ * a jump search algorithm
+ * @array: pointer to first element of array to search
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: first index containing `value`, or -1 if `value` not found or
+ * `array` is NULL
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t low, high, jump;
+
+	if (!array || size == 0)
+		return (-1);
+
+	jump = sqrt(size);
+	high = 0;
+
+	while (high < size && array[high] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", high, array[high]);
+		low = high;
+		high += jump;
+	}
+
+	printf("Value found between indexes [%lu] and [%lu]\n", low, high);
+
+	for (low = low; low < min(high, size); low++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", low, array[low]);
+		if (array[low] == value)
+			return (low);
+	}
+
+	return (-1);
+}
